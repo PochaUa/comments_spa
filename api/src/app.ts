ac77677b0799401e6e5b96f57341016f7082ea26
PochUa <po4a123@gmail.com>
2023-03-18 12:20:04 +0200

    add docker and db

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import router from './router';
+import db from './db';
 
 const app = express();
 const port = 8080;
@@ -8,6 +9,18 @@ const port = 8080;
 app.use(cors());
 app.use(router);
 
-app.listen(port, function () {
-  console.log(`Listening on ${port}`);
+app.listen(port, async () => {
+  return db
+    .sync()
+    .then(() => {
+      console.log('Synced db.');
+    })
+    .catch((err) => {
+      console.log('Failed to sync db: ' + err.message);
+    })
+    .then(() => {
+      console.log(
+        `App is ready to accept requests on http://localhost:${port}`
+      );
+    });
 });
